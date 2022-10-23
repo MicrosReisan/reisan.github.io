3c6d89b921120e5bf1c768da8fc00e9f1daf5e61
Reisan Lewi Damara <reisan_damara@rocketmail.com>
2022-10-23 21:00:30 +0700

    darkmode

--- rjs.js
+++ rjs.js
@@ -44,32 +44,17 @@ $(document).ready(function() {
     
 });
 
-
-
-const slider = document.querySelector('.scroll');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; //scroll-fast
-  slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
-});
+var a=1;
+function darkmode(){
+  var change=document.body
+  if(a%2==1){
+    change.classList.remove("default");
+    change.classList.toggle("darkmode");
+    
+  }
+  else if(a%2==0){
+    change.classList.remove("darkmode");
+    change.classList.toggle("default");
+  }
+  a++;
+}
